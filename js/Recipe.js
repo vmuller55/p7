741f8e7e82a6806dca304b0575eccbb013af4104
Vincent Muller <v.muller55000@gmail.com>
2023-04-17 13:26:40 +0200

    eslint

--- js/Recipe.js
+++ js/Recipe.js
@@ -1,128 +1,124 @@
 export default class Recipe {
-    constructor(data) {
-        this.id = data.id,
-        this.name = data.name,
-        this.servings = data.servings,
-        this.ingredients = data.ingredients,   
-        this.time = data.time,
-        this.description = data.description,
-        this.appliance = data.appliance,
-        this.ustensils = data.ustensils
-    }
-    /**
+  constructor (data) {
+    this.id = data.id
+    this.name = data.name
+    this.servings = data.servings
+    this.ingredients = data.ingredients
+    this.time = data.time
+    this.description = data.description
+    this.appliance = data.appliance
+    this.ustensils = data.ustensils
+  }
+
+  /**
      * Création du code HTML relatif aux cards des recettes
      * @returns la balise article qui englobe toute la card
      */
-    createDom() {
-        const article = document.createElement("article");
-        article.classList.add("recipeCard");
+  createDom () {
+    const article = document.createElement('article')
+    article.classList.add('recipeCard')
 
-        const img = document.createElement("img");
-        const nameAndTime = document.createElement("div"); 
-        nameAndTime.classList.add("nameAndTime");
-        article.appendChild(img);
-        article.appendChild(nameAndTime);
+    const img = document.createElement('img')
+    const nameAndTime = document.createElement('div')
+    nameAndTime.classList.add('nameAndTime')
+    article.appendChild(img)
+    article.appendChild(nameAndTime)
 
-        const h3 = document.createElement("h3");
-        h3.textContent = this.name;
-        nameAndTime.appendChild(h3);
+    const h3 = document.createElement('h3')
+    h3.textContent = this.name
+    nameAndTime.appendChild(h3)
 
-        const timeAndiconWrapper = document.createElement("div");
-        timeAndiconWrapper.classList.add("timeAndIconWrapper");
-        const icon = document.createElement("i");
-        icon.className = "fa-regular fa-clock";
-        const time = document.createElement("p");
-        time.classList.add("time");
-        time.textContent = this.time + " min";
-        nameAndTime.appendChild(timeAndiconWrapper);
-        timeAndiconWrapper.appendChild(icon);
-        timeAndiconWrapper.appendChild(time);
+    const timeAndiconWrapper = document.createElement('div')
+    timeAndiconWrapper.classList.add('timeAndIconWrapper')
+    const icon = document.createElement('i')
+    icon.className = 'fa-regular fa-clock'
+    const time = document.createElement('p')
+    time.classList.add('time')
+    time.textContent = this.time + ' min'
+    nameAndTime.appendChild(timeAndiconWrapper)
+    timeAndiconWrapper.appendChild(icon)
+    timeAndiconWrapper.appendChild(time)
 
-        const toDoWrapper = document.createElement("div");
-        toDoWrapper.classList.add("toDoWrapper");
-        article.appendChild(toDoWrapper);
+    const toDoWrapper = document.createElement('div')
+    toDoWrapper.classList.add('toDoWrapper')
+    article.appendChild(toDoWrapper)
 
-        const detail = document.createElement("div");
-        detail.classList.add("detail");
-        toDoWrapper.appendChild(detail);
+    const detail = document.createElement('div')
+    detail.classList.add('detail')
+    toDoWrapper.appendChild(detail)
 
-        const ingredients = this.ingredients;
+    const ingredients = this.ingredients
 
-        ingredients.forEach(ingredient => {
+    ingredients.forEach(ingredient => {
+      const ingredientName = document.createElement('h4')
 
-                const ingredientName = document.createElement("h4");
-                
-                if(!ingredient.quantity) {
-                    ingredientName.textContent = ingredient.ingredient;
-                }
-                else {
-                     ingredientName.textContent = ingredient.ingredient + " : ";
-                }
+      if (!ingredient.quantity) {
+        ingredientName.textContent = ingredient.ingredient
+      } else {
+        ingredientName.textContent = ingredient.ingredient + ' : '
+      }
 
-                detail.appendChild(ingredientName);
+      detail.appendChild(ingredientName)
 
-                const ingredientQuantity = document.createElement("span");
+      const ingredientQuantity = document.createElement('span')
 
-                if(!ingredient.unit) {
-                    ingredientQuantity.textContent = ingredient.quantity;
-                }
-                else {
-                    if(ingredient.unit == "grammes") {
-                        ingredientQuantity.textContent = ingredient.quantity + " g";
-                    }
-                    else {
-                        ingredientQuantity.textContent = ingredient.quantity + " " + ingredient.unit;
-                    }
-                }
-                ingredientName.appendChild(ingredientQuantity);
-            }
-        );
-        
-        const description = document.createElement("div");
-        description.classList.add("description");
-        const descriptionText = document.createElement("p");
-        descriptionText.textContent = this.description;
-        toDoWrapper.appendChild(description);
-        description.appendChild(descriptionText);
-
-        return article;
-
+      if (!ingredient.unit) {
+        ingredientQuantity.textContent = ingredient.quantity
+      } else {
+        if (ingredient.unit === 'grammes') {
+          ingredientQuantity.textContent = ingredient.quantity + ' g'
+        } else {
+          ingredientQuantity.textContent = ingredient.quantity + ' ' + ingredient.unit
+        }
+      }
+      ingredientName.appendChild(ingredientQuantity)
     }
+    )
 
-    /**
+    const description = document.createElement('div')
+    description.classList.add('description')
+    const descriptionText = document.createElement('p')
+    descriptionText.textContent = this.description
+    toDoWrapper.appendChild(description)
+    description.appendChild(descriptionText)
+
+    return article
+  }
+
+  /**
      * Permet de retourner vrai si la valeur recherchée est incluse dans le nom de la recette
-     * @param {string} nameSearch 
+     * @param {string} nameSearch
      * @returns true/false
      */
-    includeName(nameSearch) {
-        return this.name.toLowerCase().includes(nameSearch.toLowerCase());   
-    }
+  includeName (nameSearch) {
+    return this.name.toLowerCase().includes(nameSearch.toLowerCase())
+  }
 
-    includeDescription(descriptionSearch) {
-        return this.description.toLowerCase().includes(descriptionSearch.toLowerCase());  
-    }
+  includeDescription (descriptionSearch) {
+    return this.description.toLowerCase().includes(descriptionSearch.toLowerCase())
+  }
 
-    includeIngredient(ingredientSearch) {
-        let result = false;
-        this.ingredients.forEach(ingredient => {
-            if(ingredient.ingredient.toLowerCase().includes(ingredientSearch.toLowerCase())) {
-                result = true;
-            }
-        }) 
-        return result;  
-    }
+  includeIngredient (ingredientSearch) {
+    let result = false
+    this.ingredients.forEach(ingredient => {
+      if (ingredient.ingredient.toLowerCase().includes(ingredientSearch.toLowerCase())) {
+        result = true
+      }
+    })
+    return result
+  }
 
-    includeAppliance(applianceSearch) {
-        return this.appliance.toLowerCase().includes(applianceSearch.toLowerCase());  
-    }
+  includeAppliance (applianceSearch) {
+    return this.appliance.toLowerCase().includes(applianceSearch.toLowerCase())
+  }
 
-    includeUstensil(ustensilSearch) {
-        let result = false;
-        this.ustensils.forEach(ustensil => {
-            if(ustensil.toLowerCase().includes(ustensilSearch.toLowerCase())) {
-                result= true;
-            }
-        })
-        return result;
-    }
+  includeUstensil (ustensilSearch) {
+    let result = false
+    this.ustensils.forEach(ustensil => {
+      if (ustensil.toLowerCase().includes(ustensilSearch.toLowerCase())) {
+        result = true
+      }
+    })
+    return result
+  }
 }
--- js/research.js
+++ js/research.js
@@ -1,9 +1,10 @@
-const recipeSection = document.getElementById("recipeCards");
+/* eslint-disable import/no-duplicates */
 /**
  * Importation des fonctions nécéssaires
  */
-import { displaySearchRecipes } from './index.js';
-import {createTagDom} from './index.js';
+import { displaySearchRecipes } from './index.js'
+import { createTagDom } from './index.js'
+const recipeSection = document.getElementById('recipeCards')
 /**
  * Permet d'initialiser la fonction de recherche par tag ou avec la barre de recherche selon plusieurs conditions
  * @param {Array} recipes Les recettes dans lesquels faire la recherche
@@ -13,98 +14,94 @@ import {createTagDom} from './index.js';
  * @param {Array} deviceTagCheckedTab le tableau des appareils séléctionnés
  * @returns le resultat des recherches dans la variable resultat
  */
-export function principalSearch(recipes, inputValue, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab){
-    /**
+export function principalSearch (recipes, inputValue, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab) {
+  /**
      * Permet de récupérer la valeur sans espaces autour
      */
-    inputValue = inputValue.trim();
-    /**
+  inputValue = inputValue.trim()
+  /**
      * Tableau qui va recueillir les recettes correspondantes à la recherche
-     */  
-    let result = recipes;
-    /**
+     */
+  let result = recipes
+  /**
      * Permet d'afficher l'ensemble des recettes lorsque aucun tag n'est sélectionné et aucun que la barre de recherche est vide
      */
-    if(!inputValue.length && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) { 
-        return displaySearchRecipes(recipes);
-    }
-    if(inputValue.length >= 3 ) {
-        result = [];
-        /**
+  if (!inputValue.length && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) {
+    return displaySearchRecipes(recipes)
+  }
+  if (inputValue.length >= 3) {
+    result = []
+    /**
          * Utilisation de la classe Recipe pour vérifier si la recette contient la valeur recherchée dans son nom, sa description ou ses ingrédients
          */
-        recipes.forEach(recipe => {
-            if(recipe.includeName(inputValue) || recipe.includeDescription(inputValue) || recipe.includeIngredient(inputValue)) {
-                result.push(recipe);        
-            }
-        })
-        
-    }
-    let lastResult = result;
+    recipes.forEach(recipe => {
+      if (recipe.includeName(inputValue) || recipe.includeDescription(inputValue) || recipe.includeIngredient(inputValue)) {
+        result.push(recipe)
+      }
+    })
+  }
+  let lastResult = result
 
-    if(ingredientTagCheckedTab.length || ustensilTagCheckedTab.length || deviceTagCheckedTab.length) {
-        lastResult = [];
-        result.forEach(recipe=> {
-            if(ingredientTagCheckedTab.every(ingredient => recipe.includeIngredient(ingredient)) && ustensilTagCheckedTab.every(ustensil => recipe.includeUstensil(ustensil)) && deviceTagCheckedTab.every(device => recipe.includeAppliance(device))) {
-                lastResult.push(recipe)
-            }
-        })   
-    }
+  if (ingredientTagCheckedTab.length || ustensilTagCheckedTab.length || deviceTagCheckedTab.length) {
+    lastResult = []
+    result.forEach(recipe => {
+      if (ingredientTagCheckedTab.every(ingredient => recipe.includeIngredient(ingredient)) && ustensilTagCheckedTab.every(ustensil => recipe.includeUstensil(ustensil)) && deviceTagCheckedTab.every(device => recipe.includeAppliance(device))) {
+        lastResult.push(recipe)
+      }
+    })
+  }
 
-
-    if(lastResult.length) {
-        lastResult = [...new Set(lastResult)].sort();
-        return displaySearchRecipes(lastResult)
-    }
-    else{
-        /**
+  if (lastResult.length) {
+    lastResult = [...new Set(lastResult)].sort()
+    return displaySearchRecipes(lastResult)
+  } else {
+    /**
          * Sinon, si la valeur de la barre de recherche est supérieure à 3 et qu'aucun résultat n'est trouvé, affichage d'un message
          */
-        if(inputValue.length >= 3) {
-            return noRecipe();
-        }
+    if (inputValue.length >= 3) {
+      return noRecipe()
     }
+  }
 }
 /**
  * Fonction qui permet d'afficher un message lorsqu'aucune recette n'est trouvée
  */
 function noRecipe () {
-    recipeSection.innerHTML = '';
-    const displayMessage = document.createElement('h3');
-    displayMessage.classList.add('displayMessage');
-    displayMessage.textContent = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc';
-    recipeSection.appendChild(displayMessage);
-
+  recipeSection.innerHTML = ''
+  const displayMessage = document.createElement('h3')
+  displayMessage.classList.add('displayMessage')
+  displayMessage.textContent = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc'
+  recipeSection.appendChild(displayMessage)
 }
 /**
  * Fonction permettant d'effectuer une recherche dans la liste des tags sélectionnables
- * @param {Array} ingredientsTab 
- * @param {string} inputValue 
+ * @param {Array} ingredientsTab
+ * @param {string} inputValue
  */
-export function searchInIngredientTag(ingredientsTab, inputValue){
-    const ingredientList = document.getElementById('ingredientsList');
-    ingredientList.innerHTML = '';
-    ingredientsTab.forEach(findIngredient => {
-        if(findIngredient.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findIngredient, 'ingredients');
-        }
-    })
+export function searchInIngredientTag (ingredientsTab, inputValue) {
+  const ingredientList = document.getElementById('ingredientsList')
+  ingredientList.innerHTML = ''
+  ingredientsTab.forEach(findIngredient => {
+    if (findIngredient.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findIngredient, 'ingredients')
+    }
+  })
 }
-export function searchInDeviceTag(deviceTab, inputValue){
-    const deviceList = document.getElementById('devicesList');
-    deviceList.innerHTML = '';
-    deviceTab.forEach(findDevice => {
-        if(findDevice.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findDevice, 'devices');
-        }
-    })
+export function searchInDeviceTag (deviceTab, inputValue) {
+  const deviceList = document.getElementById('devicesList')
+  deviceList.innerHTML = ''
+  deviceTab.forEach(findDevice => {
+    if (findDevice.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findDevice, 'devices')
+    }
+  })
 }
-export function searchInUstensilTag(ustensilsTab, inputValue){
-    const ustensilList = document.getElementById('ustensilsList');
-    ustensilList.innerHTML = '';
-    ustensilsTab.forEach(findUstensil => {
-        if(findUstensil.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findUstensil, 'ustensils');
-        }
-    })
+export function searchInUstensilTag (ustensilsTab, inputValue) {
+  const ustensilList = document.getElementById('ustensilsList')
+  ustensilList.innerHTML = ''
+  ustensilsTab.forEach(findUstensil => {
+    if (findUstensil.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findUstensil, 'ustensils')
+    }
+  })
 }
