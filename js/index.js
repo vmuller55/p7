8482a8ec9d87f8000cfd024a1cea7863b7a45e40
Vincent Muller <v.muller55000@gmail.com>
2023-04-17 18:42:58 +0200

    eslint

--- js/index.js
+++ js/index.js
@@ -1,235 +1,261 @@
-import { recipes } from "./recipes.js";
-import Recipe from "./Recipe.js";
-import { principalSearch, searchInIngredientTag, searchInDeviceTag, searchInUstensilTag } from "./research.js";
+import { recipes } from './recipes.js'
+import Recipe from './Recipe.js'
+import { principalSearch, searchInIngredientTag, searchInDeviceTag, searchInUstensilTag } from './research.js'
 
 /**
  * Tableau dans lequel on va initialiser toutes les recettes
  */
-export const allRecipe = [];
+export const allRecipe = []
 /**
  * Permet de gérer les inputs et l'ouverture de la liste des filtres
  */
-const searchBar = document.getElementById("searchBar");
-const ingredientListOpen = document.getElementById('ingredientsDown');
-const ingredientListClose = document.getElementById('ingredientsUp');
-const inputIngredient = document.getElementById('inputIngredient');
-const deviceListOpen = document.getElementById('devicesDown');
-const deviceListClose = document.getElementById('devicesUp');
-const inputDevice = document.getElementById('inputDevice');
-const ustensilListOpen = document.getElementById('ustensilsDown');
-const ustensilListClose = document.getElementById('ustensilsUp');
-const inputUstensil = document.getElementById('inputUstensil');
+const searchBar = document.getElementById('searchBar')
+const ingredientListOpen = document.getElementById('ingredientsDown')
+const ingredientListClose = document.getElementById('ingredientsUp')
+const inputIngredient = document.getElementById('inputIngredient')
+const deviceListOpen = document.getElementById('devicesDown')
+const deviceListClose = document.getElementById('devicesUp')
+const inputDevice = document.getElementById('inputDevice')
+const ustensilListOpen = document.getElementById('ustensilsDown')
+const ustensilListClose = document.getElementById('ustensilsUp')
+const inputUstensil = document.getElementById('inputUstensil')
 /**
  * Tableaux des filtres séléctionnés
  */
-var ingredientTagCheckedTab = [];
-var ustensilTagCheckedTab = [];
-var deviceTagCheckedTab = [];
+const ingredientTagCheckedTab = []
+const ustensilTagCheckedTab = []
+const deviceTagCheckedTab = []
 /**
  * Tableaux des filtres séléctionnable
  */
-var ingredientsTab = [];
-var ustensilsTab = [];
-var devicesTab = [];
+let ingredientsTab = []
+let ustensilsTab = []
+let devicesTab = []
 /**
  * Utilise la classe Recipe pour le premier affichage des recettes
  * @param {Array} recipes Tableaux des recettes séléctionnées
  */
-function displayRecipe(recipes) {
-    let recipeSection = document.getElementById("recipeCards");
-    recipeSection.innerHTML = '';
-    for(let i = 0; i < recipes.length; i++){
-        const createRecipe = new Recipe(recipes[i]);
-        allRecipe.push(createRecipe);
-        const createDom = createRecipe.createDom();
-        recipeSection.appendChild(createDom);
-    }
-    createTabTag((recipes))
+function displayRecipe (recipes) {
+  const recipeSection = document.getElementById('recipeCards')
+  recipeSection.innerHTML = ''
+  for (let i = 0; i < recipes.length; i++) {
+    const createRecipe = new Recipe(recipes[i])
+    allRecipe.push(createRecipe)
+    const createDom = createRecipe.createDom()
+    recipeSection.appendChild(createDom)
+  }
+  createTabTag((recipes))
 }
 /**
  * Permet d'afficher les recettes recherchées
- * @param {Array} recipes 
+ * @param {Array} recipes
  */
-export function displaySearchRecipes(recipes) {
-    let recipeSection = document.getElementById("recipeCards");
-    recipeSection.innerHTML = '';
-    for(let i = 0; i < recipes.length; i++) {
-        const createDom = recipes[i].createDom();
-        recipeSection.appendChild(createDom);
-    }
-    createTabTag(recipes)  
+export function displaySearchRecipes (recipes) {
+  const recipeSection = document.getElementById('recipeCards')
+  recipeSection.innerHTML = ''
+  for (let i = 0; i < recipes.length; i++) {
+    const createDom = recipes[i].createDom()
+    recipeSection.appendChild(createDom)
+  }
+  createTabTag(recipes)
 }
 /**
  * Permet d'initialiser la fonction de tri et la fonction relative à la création des éléments
- * @param {Array} recipes 
+ * @param {Array} recipes
  */
-export function createTabTag(recipes) {
-    filterTabTag(recipes);
-    ingredientsTab.forEach((ingredient) => {
-        createTagDom(ingredient, 'ingredients');   
-    })
-    ustensilsTab.forEach((ustensil) => {
-        createTagDom(ustensil, 'ustensils');
-    })
-    devicesTab.forEach((device) => {
-        createTagDom(device, 'devices');
-    })
+export function createTabTag (recipes) {
+  filterTabTag(recipes)
+  ingredientsTab.forEach((ingredient) => {
+    createTagDom(ingredient, 'ingredients')
+  })
+  ustensilsTab.forEach((ustensil) => {
+    createTagDom(ustensil, 'ustensils')
+  })
+  devicesTab.forEach((device) => {
+    createTagDom(device, 'devices')
+  })
 }
 /**
  * Permet de filtrer les éléments séléctionables dans les filtres
- * @param {Array} recipes 
+ * @param {Array} recipes
  */
-function filterTabTag(recipes) {
-    ingredientsTab = [];
-    ustensilsTab = [];
-    devicesTab = [];
-    /**
+function filterTabTag (recipes) {
+  ingredientsTab = []
+  ustensilsTab = []
+  devicesTab = []
+  /**
      * permet d'afficher l'ensemble des éléments si aucun tag est séléctionné et si aucune valeur est présente dans la barre de recherche
      */
-    if(!searchBar.value && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) {
-        recipes = allRecipe
-    }
-    /**
+  if (!searchBar.value && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) {
+    recipes = allRecipe
+  }
+  /**
      * Supression du précédent contenu des listes
      */
-    let ingredientList = document.getElementById('ingredientsList');
-    let deviceList = document.getElementById('devicesList');
-    let ustensilList = document.getElementById('ustensilsList');
-    ingredientList.innerHTML = '';
-    deviceList.innerHTML = '';
-    ustensilList.innerHTML = '';
-    /**
+  const ingredientList = document.getElementById('ingredientsList')
+  const deviceList = document.getElementById('devicesList')
+  const ustensilList = document.getElementById('ustensilsList')
+  ingredientList.innerHTML = ''
+  deviceList.innerHTML = ''
+  ustensilList.innerHTML = ''
+  /**
      * Ajout des éléments selon les recettes
      */
-    recipes.forEach(recipe => {
-        recipe.ingredients.forEach((ingredient) => { 
-            ingredientsTab.push(ingredient.ingredient.charAt(0).toUpperCase() + ingredient.ingredient.slice(1).toLowerCase());
-        })
-        recipe.ustensils.forEach((ustensil) => { 
-            ustensilsTab.push(ustensil.charAt(0).toUpperCase() + ustensil.slice(1).toLowerCase());
-        });
-        devicesTab.push(recipe.appliance.charAt(0).toUpperCase() + recipe.appliance.slice(1).toLowerCase());
-    });
-    /**
+  recipes.forEach(recipe => {
+    recipe.ingredients.forEach((ingredient) => {
+      ingredientsTab.push(ingredient.ingredient.charAt(0).toUpperCase() + ingredient.ingredient.slice(1).toLowerCase())
+    })
+    recipe.ustensils.forEach((ustensil) => {
+      ustensilsTab.push(ustensil.charAt(0).toUpperCase() + ustensil.slice(1).toLowerCase())
+    })
+    devicesTab.push(recipe.appliance.charAt(0).toUpperCase() + recipe.appliance.slice(1).toLowerCase())
+  })
+  /**
      * Permet de supprimer les doublons et de trier par ordre alphabetique
      */
-    ingredientsTab = [...new Set(ingredientsTab)].sort();
-    ustensilsTab = [...new Set(ustensilsTab)].sort();
-    devicesTab = [...new Set(devicesTab)].sort();
-    /**
+  ingredientsTab = [...new Set(ingredientsTab)].sort()
+  ustensilsTab = [...new Set(ustensilsTab)].sort()
+  devicesTab = [...new Set(devicesTab)].sort()
+  /**
      * Permet de supprimer un tag déjà séléctionné de la liste
      */
-    removeExistingTagFromList(ingredientTagCheckedTab, ingredientsTab);
-    removeExistingTagFromList(ustensilTagCheckedTab,ustensilsTab);
-    removeExistingTagFromList(deviceTagCheckedTab, devicesTab);
+  removeExistingTagFromList(ingredientTagCheckedTab, ingredientsTab)
+  removeExistingTagFromList(ustensilTagCheckedTab, ustensilsTab)
+  removeExistingTagFromList(deviceTagCheckedTab, devicesTab)
+}
+function removeExistingTagFromList (array, list) {
+  array.forEach((arrayElement) => {
+    list.forEach((listElement) => {
+      if (listElement === arrayElement) {
+        removeElementFromArray(list, listElement)
+      }
+    })
+  })
+}
+
+function removeElementFromArray (array, element) {
+  const indexOfElement = array.indexOf(element)
+  array.splice(indexOfElement, 1)
 }
 /**
  * Permet de créer un élément de la liste des filtres
- * @param {string} element 
- * @param {string} type 
+ * @param {string} element
+ * @param {string} type
  */
-export function createTagDom(element, type) {
-    let ulLocation = document.getElementById(type + 'List');
-    let liTag = document.createElement('li');
-    liTag.textContent = element;
-    ulLocation.appendChild(liTag);
-    /**
+export function createTagDom (element, type) {
+  const ulLocation = document.getElementById(type + 'List')
+  const liTag = document.createElement('li')
+  liTag.textContent = element
+  ulLocation.appendChild(liTag)
+  /**
      * Permet de gérer les evenements au clic sut le tag
      */
-    liTag.addEventListener('click',() => {
-        addTag(element, type);
-        addTagInTab(element, type);
-        principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab);
-    }) 
+  liTag.addEventListener('click', () => {
+    addTag(element, type)
+    addTagInTab(element, type)
+    principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab)
+  })
 }
 /**
- * Permet d'ajouter un tag dans la liste au tableau de tag séléctionnés correspondant 
- * @param {string} element 
- * @param {string} type 
+ * Permet d'ajouter un tag dans la liste au tableau de tag séléctionnés correspondant
+ * @param {string} element
+ * @param {string} type
  */
-function addTagInTab(element, type) {
-    switch (type) {
-        case 'ingredients' :
-            ingredientTagCheckedTab.push(element);
-            break;
-        case 'ustensils' :
-            ustensilTagCheckedTab.push(element);
-            break;
-        case 'devices' :
-            deviceTagCheckedTab.push(element);
-            break;
-    };
+function addTagInTab (element, type) {
+  switch (type) {
+    case 'ingredients' :
+      ingredientTagCheckedTab.push(element)
+      break
+    case 'ustensils' :
+      ustensilTagCheckedTab.push(element)
+      break
+    case 'devices' :
+      deviceTagCheckedTab.push(element)
+      break
+  };
 }
 /**
  * Permet de créer le tag séléctionné
- * @param {string} element 
- * @param {string} type 
+ * @param {string} element
+ * @param {string} type
  */
-function addTag(element, type) {
-    let ulLocation = document.getElementById('tagChecked');
-    let liTag = document.createElement('li');
-    liTag.classList.add(type + 'TagChecked')
-    liTag.textContent = element;
-    let closeCross = document.createElement('i');
-    closeCross.setAttribute('class', 'fa-sharp fa-regular fa-circle-xmark');
-    liTag.appendChild(closeCross);
-    ulLocation.appendChild(liTag);
-    /**
+function addTag (element, type) {
+  const ulLocation = document.getElementById('tagChecked')
+  const liTag = document.createElement('li')
+  liTag.classList.add(type + 'TagChecked')
+  liTag.textContent = element
+  const closeCross = document.createElement('i')
+  closeCross.setAttribute('class', 'fa-sharp fa-regular fa-circle-xmark')
+  liTag.appendChild(closeCross)
+  ulLocation.appendChild(liTag)
+  /**
      * Gestion de la suppréssion du tag
      */
-    closeCross.addEventListener('click', () => {
-        liTag.remove(),
-        removeTag(type, element);
-        principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab);
-    });
+  closeCross.addEventListener('click', () => {
+    liTag.remove()
+    removeTag(type, element)
+    principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab)
+  })
 }
 /**
  * Permet de supprimer le tag du tableau des tags séléctionnés et de faire une recherche sur les tags restant si il y en a.
- * @param {string} type 
- * @param {string} element 
+ * @param {string} type
+ * @param {string} element
  */
 function removeTag (type, element) {
-    /**
+  /**
      * Suppréssion des tags dans les tableaux
      */
-    switch (type) {
-        case 'ingredients' :
-            removeElementFromArray(ingredientTagCheckedTab, element);
-            break;
-        case 'ustensils' :
-            removeElementFromArray(ustensilTagCheckedTab, element);
-            break;
-        case 'devices' :
-            removeElementFromArray(deviceTagCheckedTab, element); 
-        break;
-    }
+  switch (type) {
+    case 'ingredients' :
+      removeElementFromArray(ingredientTagCheckedTab, element)
+      break
+    case 'ustensils' :
+      removeElementFromArray(ustensilTagCheckedTab, element)
+      break
+    case 'devices' :
+      removeElementFromArray(deviceTagCheckedTab, element)
+      break
+  }
+}
+function openList (type) {
+  const whichList = document.querySelector('.' + type + 'List')
+  whichList.style.display = 'block'
+  const whichBtn = document.querySelector('.' + type + 'Btn')
+  whichBtn.style.width = '600px'
 }
 
+function closeList (type) {
+  const whichList = document.querySelector('.' + type + 'List')
+  whichList.style.display = 'none'
+  const whichBtn = document.querySelector('.' + type + 'Btn')
+  whichBtn.style.width = 'unset'
+}
 /**
  * Initialisation des fonctions primaires
  */
-function init() {
-    displayRecipe(recipes);
+function init () {
+  displayRecipe(recipes)
 }
 
-init();
+init()
 /**
  * Gestion des événements sur les différents input
  */
-searchBar.addEventListener("input", () => {
-    principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab)
-});
-searchBar.addEventListener("keypress", (e) => {if(e.key === 'Enter') { e.preventDefault()}});
+searchBar.addEventListener('input', () => {
+  principalSearch(allRecipe, searchBar.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab)
+})
+searchBar.addEventListener('keypress', (e) => { if (e.key === 'Enter') { e.preventDefault() } })
 
-ingredientListOpen.addEventListener('click', () => {openList('ingredient'), closeList('device'), closeList('ustensil')});
-ingredientListClose.addEventListener('click', () => {closeList('ingredient')});
+ingredientListOpen.addEventListener('click', () => { openList('ingredient'); closeList('device'); closeList('ustensil') })
+ingredientListClose.addEventListener('click', () => { closeList('ingredient') })
 
-deviceListOpen.addEventListener('click', () => {openList('device'), closeList('ingredient'), closeList('ustensil')});
-deviceListClose.addEventListener('click', () => {closeList('device')});
+deviceListOpen.addEventListener('click', () => { openList('device'); closeList('ingredient'); closeList('ustensil') })
+deviceListClose.addEventListener('click', () => { closeList('device') })
 
-ustensilListOpen.addEventListener('click', () => {openList('ustensil'), closeList('device'), closeList('ingredient')});
-ustensilListClose.addEventListener('click', () => {closeList('ustensil')});
+ustensilListOpen.addEventListener('click', () => { openList('ustensil'); closeList('device'); closeList('ingredient') })
+ustensilListClose.addEventListener('click', () => { closeList('ustensil') })
 
-inputIngredient.addEventListener('input', () => {searchInIngredientTag(ingredientsTab, inputIngredient.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab)});
-inputDevice.addEventListener('input', () => {searchInDeviceTag(devicesTab, inputDevice.value)});
-inputUstensil.addEventListener('input', () => {searchInUstensilTag(ustensilsTab, inputUstensil.value)});
+inputIngredient.addEventListener('input', () => { searchInIngredientTag(ingredientsTab, inputIngredient.value, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab) })
+inputDevice.addEventListener('input', () => { searchInDeviceTag(devicesTab, inputDevice.value) })
+inputUstensil.addEventListener('input', () => { searchInUstensilTag(ustensilsTab, inputUstensil.value) })
--- js/research.js
+++ js/research.js
@@ -1,9 +1,10 @@
-const recipeSection = document.getElementById("recipeCards");
+/* eslint-disable import/no-duplicates */
 /**
  * Importation des fonctions nécéssaires
  */
-import { displaySearchRecipes } from './index.js';
-import {createTagDom} from './index.js';
+import { displaySearchRecipes } from './index.js'
+import { createTagDom } from './index.js'
+const recipeSection = document.getElementById('recipeCards')
 /**
  * Permet d'initialiser la fonction de recherche par tag ou avec la barre de recherche selon plusieurs conditions
  * @param {Array} recipes Les recettes dans lesquels faire la recherche
@@ -13,88 +14,86 @@ import {createTagDom} from './index.js';
  * @param {Array} deviceTagCheckedTab le tableau des appareils séléctionnés
  * @returns le resultat des recherches dans la variable resultat
  */
-export function principalSearch(recipes, inputValue, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab){
-    /**
+export function principalSearch (recipes, inputValue, ingredientTagCheckedTab, ustensilTagCheckedTab, deviceTagCheckedTab) {
+  /**
      * Permet de récupérer la valeur sans espaces autour
      */
-    inputValue = inputValue.trim();
-    /**
+  inputValue = inputValue.trim()
+  /**
      * Tableau qui va recueillir les recettes correspondantes à la recherche
-     */  
-    let result = recipes;
-    /**
+     */
+  let result = recipes
+  /**
      * Permet d'afficher l'ensemble des recettes lorsque aucun tag n'est sélectionné et aucun que la barre de recherche est vide
      */
-    if(!inputValue.length && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) { 
-        return displaySearchRecipes(recipes);
-    }
-    if(inputValue.length >= 3) {
-        result = [];
-        /**
+  if (!inputValue.length && !ingredientTagCheckedTab.length && !ustensilTagCheckedTab.length && !deviceTagCheckedTab.length) {
+    return displaySearchRecipes(recipes)
+  }
+  if (inputValue.length >= 3) {
+    result = []
+    /**
          * Utilisation de la classe Recipe pour vérifier si la recette contient la valeur recherchée dans son nom, sa description ou ses ingrédients
          */
-        for(let i = 0; i < recipes.length; i++) {
-            if(recipes[i].includeName(inputValue) || recipes[i].includeDescription(inputValue) || recipes[i].includeIngredient(inputValue)) {
-                result.push(recipes[i]);        
-            }
-        }
+    for (let i = 0; i < recipes.length; i++) {
+      if (recipes[i].includeName(inputValue) || recipes[i].includeDescription(inputValue) || recipes[i].includeIngredient(inputValue)) {
+        result.push(recipes[i])
+      }
     }
-    let lastResult = result;
-    if(ingredientTagCheckedTab.length || ustensilTagCheckedTab.length || deviceTagCheckedTab.length) {
-        lastResult = [];
-        for(let i =0; i < result.length; i++) {
-            if(ingredientTagCheckedTab.every(ingredient => result[i].includeIngredient(ingredient)) && ustensilTagCheckedTab.every(ustensil => result[i].includeUstensil(ustensil)) && deviceTagCheckedTab.every(device => result[i].includeAppliance(device))) {
-                lastResult.push(result[i])
-            }
-        }
+  }
+  let lastResult = result
+  if (ingredientTagCheckedTab.length || ustensilTagCheckedTab.length || deviceTagCheckedTab.length) {
+    lastResult = []
+    for (let i = 0; i < result.length; i++) {
+      if (ingredientTagCheckedTab.every(ingredient => result[i].includeIngredient(ingredient)) && ustensilTagCheckedTab.every(ustensil => result[i].includeUstensil(ustensil)) && deviceTagCheckedTab.every(device => result[i].includeAppliance(device))) {
+        lastResult.push(result[i])
+      }
     }
-    if(lastResult.length) {
-        lastResult = [...new Set(lastResult)].sort();
-        return displaySearchRecipes(lastResult)
-    }
-    else{
-        /**
+  }
+  if (lastResult.length) {
+    lastResult = [...new Set(lastResult)].sort()
+    return displaySearchRecipes(lastResult)
+  } else {
+    /**
          * Sinon, si la valeur de la barre de recherche est supérieure à 3 et qu'aucun résultat n'est trouvé, affichage d'un message
          */
-        if(inputValue.length >= 3) {
-            return noRecipe();
-        }
+    if (inputValue.length >= 3) {
+      return noRecipe()
     }
+  }
 }
 
 function noRecipe () {
-    recipeSection.innerHTML = '';
-    const displayMessage = document.createElement('h3');
-    displayMessage.classList.add('displayMessage');
-    displayMessage.textContent = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc';
-    recipeSection.appendChild(displayMessage);
-
+  recipeSection.innerHTML = ''
+  const displayMessage = document.createElement('h3')
+  displayMessage.classList.add('displayMessage')
+  displayMessage.textContent = 'Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc'
+  recipeSection.appendChild(displayMessage)
 }
 
-export function searchInIngredientTag(ingredientsTab, inputValue){
-    const ingredientList = document.getElementById('ingredientsList');
-    ingredientList.innerHTML = '';
-    ingredientsTab.forEach(findIngredient => {
-        if(findIngredient.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findIngredient, 'ingredients');
-        }
-    })
+export function searchInIngredientTag (ingredientsTab, inputValue) {
+  const ingredientList = document.getElementById('ingredientsList')
+  ingredientList.innerHTML = ''
+  ingredientsTab.forEach(findIngredient => {
+    if (findIngredient.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findIngredient, 'ingredients')
+    }
+  })
 }
-export function searchInDeviceTag(deviceTab, inputValue){
-    const deviceList = document.getElementById('devicesList');
-    deviceList.innerHTML = '';
-    deviceTab.forEach(findDevice => {
-        if(findDevice.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findDevice, 'devices');
-        }
-    })
+export function searchInDeviceTag (deviceTab, inputValue) {
+  const deviceList = document.getElementById('devicesList')
+  deviceList.innerHTML = ''
+  deviceTab.forEach(findDevice => {
+    if (findDevice.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findDevice, 'devices')
+    }
+  })
 }
-export function searchInUstensilTag(ustensilsTab, inputValue){
-    const ustensilList = document.getElementById('ustensilsList');
-    ustensilList.innerHTML = '';
-    ustensilsTab.forEach(findUstensil => {
-        if(findUstensil.toLowerCase().includes(inputValue.toLowerCase())) {
-            createTagDom(findUstensil, 'ustensils');
-        }
-    })
+export function searchInUstensilTag (ustensilsTab, inputValue) {
+  const ustensilList = document.getElementById('ustensilsList')
+  ustensilList.innerHTML = ''
+  ustensilsTab.forEach(findUstensil => {
+    if (findUstensil.toLowerCase().includes(inputValue.toLowerCase())) {
+      createTagDom(findUstensil, 'ustensils')
+    }
+  })
 }
